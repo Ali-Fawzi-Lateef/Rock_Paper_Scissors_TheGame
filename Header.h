befac8d6834645b696a68e9dfc6f2a39490fc1b7
Ali <83194640+kenpachi01011@users.noreply.github.com>
2021-09-08 00:13:05 +0300

    Update Header.h

--- Header.h
+++ Header.h
@@ -5,8 +5,8 @@ class RPS // stands for "Rock Paper Scissors"
 {
 	int choiec;
 public:
-	RPS(std::string selection);
-	void SetValue(int x);
+	RPS(std::string selection); //constructor takes the user input.
+	void SetValue(int x); //function for seting the input value to choiec.
 	int GenerateNum();
 	void ChechWin();
 	void Result();
