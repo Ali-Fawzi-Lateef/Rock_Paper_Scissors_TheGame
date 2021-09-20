84ab3225713476fb41d43ea0fdc72d7e739f5a73
Ali <83194640+kenpachi01011@users.noreply.github.com>
2021-09-20 21:04:28 +0300

    Update Header.h

--- Header.h
+++ Header.h
@@ -1,15 +1,15 @@
 #ifndef HEADER
 #define HEADER
 #include <string>
-class RPS // stands for "Rock Paper Scissors".
+class RPS // stands for "Rock Paper Scissors"
 {
 	int choiec;
 public:
-	RPS(std::string selection); //constructor takes the user input.
-	void SetValue(int x); // seting the input value to choiec.
-	int GenerateNum(); // generating a random number as a computer selection.
-	void CheckWin(); // checking who won.
-	void Result(); // printing the final result.
+	RPS(std::string selection); //constructor takes the user input
+	void SetValue(int x); // seting the input value to choiec
+	int GenerateNum(); // generating a random number as a computer selection
+	void CheckWin(); // checking who won
+	void Result(); // printing the final result
 };
 #endif 
 
